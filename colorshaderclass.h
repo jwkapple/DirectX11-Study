0bbfff4238948519faf96b4ea49dcf8de6c07d2e
jwkapple <jwkorange@gmail.com>
2019-02-02 14:20:49 +0900

    modern tut04 coded with window SDK & visual studio 2017

---
+++ colorshaderclass.h
@@ -0,0 +1,45 @@
+#pragma once
+#pragma comment(lib,"d3dcompiler.lib")
+
+#include <d3d11.h>
+#include <DirectXMath.h>
+#include <fstream>
+#include <d3dcompiler.h>
+using namespace DirectX;
+using namespace std;
+
+class ColorShaderClass
+{
+private:
+	struct MatrixBufferType
+	{
+		XMMATRIX world;
+		XMMATRIX view;
+		XMMATRIX projection;
+	};
+
+public:
+	ColorShaderClass();
+	ColorShaderClass(const ColorShaderClass&);
+	~ColorShaderClass();
+
+	bool Initialize(ID3D11Device*, HWND);
+	void Shutdown();
+	// 셰이더에 사용되는 변수들을 설정하고 셰이더를 이용해 준비된 모델의 정점들을 그려냅니다.
+	bool Render(ID3D11DeviceContext*, int, XMMATRIX, XMMATRIX, XMMATRIX);
+
+private:
+	bool InitializeShader(ID3D11Device*, HWND, WCHAR*, WCHAR*);
+	void ShutdownShader();
+	void OutputShaderErrorMessage(ID3D10Blob*, HWND, WCHAR*);
+
+	bool SetShaderParameters(ID3D11DeviceContext*, XMMATRIX, XMMATRIX, XMMATRIX);
+	void RenderShader(ID3D11DeviceContext*, int);
+
+private:
+	ID3D11VertexShader* m_vertexShader;
+	ID3D11PixelShader* m_pixelShader;
+	ID3D11InputLayout* m_layout;
+	ID3D11Buffer* m_matrixBuffer;
+
+};
---
+++ graphicsclass.cpp
@@ -0,0 +1,141 @@
+
+#include "graphicsclass.h"
+
+
+
+GraphicsClass::GraphicsClass()
+{
+	m_D3D = 0;
+
+	
+}
+
+GraphicsClass::GraphicsClass(const GraphicsClass& other)
+{
+
+}
+
+GraphicsClass::~GraphicsClass()
+{
+
+}
+
+bool GraphicsClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
+{
+	bool result;
+
+	//  Direct3D 객체를 생성합니다.
+	m_D3D = new D3DClass;
+	if (!m_D3D)
+	{
+		return false;
+	}
+
+	// Direct3D 객체를 초기화합니다.
+	result = m_D3D->Initialize(screenWidth, screenHeight, VSYNC_ENABLED, hwnd, FULL_SCREEN, SCREEN_DEPTH, SCREEN_NEAR);
+	if (!result)
+	{
+		MessageBox(hwnd, (LPCSTR)L"Could not initialize Direct3D", (LPCSTR)L"Error", MB_OK);
+		return false;
+	}
+
+	// Create the Camera Object.
+	m_Camera = new CameraClass;
+	if (!m_Camera)
+	{
+		return false;
+	}
+
+	// Set the initial position of the camera.
+	m_Camera->SetPosition(0.0f, 0.0f, -10.0f);
+
+	// Create the model object.
+	m_Model = new ModelClass;
+	if (!m_Model)
+	{
+		return false;
+	}
+
+	result = m_Model->Initialize(m_D3D->GetDevice());
+	if (!result)
+	{
+		MessageBox(hwnd, (LPCSTR)L"Could not initialize the model obejct", (LPCSTR)L"Error", MB_OK);
+		return false;
+	}
+
+	// Create the color shader object.
+	m_ColorShader = new ColorShaderClass;
+	if (!m_ColorShader)
+	{
+		return false;
+	}
+
+	result = m_ColorShader->Initialize(m_D3D->GetDevice(), hwnd);
+	if (!result)
+	{
+		MessageBox(hwnd, (LPCSTR)"Aould not initialize the shader obejct", (LPCSTR)"Error", MB_OK);
+		return false;
+	}
+	
+	
+	return true;
+}
+
+void GraphicsClass::Shutdown()
+{
+	// D3D 객체를 변환합니다.
+	if (m_D3D)
+	{
+		m_D3D->Shutdown();
+		delete m_D3D;
+		m_D3D = 0;
+	}
+
+
+	return;
+}
+
+bool GraphicsClass::Frame()
+{
+	bool result;
+
+	//그래픽 렌더링을 수행합니다.
+	result = Render();
+	if (!result)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+bool GraphicsClass::Render()
+{	// 씬 그리기를 시작하기 위해 버퍼의 내용을 지웁니다.
+	m_D3D->BeginScene(0.0f, 0.0f, 1.0f,1.0f);
+	
+	// Generate the view matrix based on the camera's position.
+	m_Camera->Render();
+
+
+	XMMATRIX worldMatrix = XMMatrixIdentity(), viewMatrix = XMMatrixIdentity(), projectionMatrix = XMMatrixIdentity();
+	bool result;
+
+	m_Camera->GetViewMatrix(viewMatrix);
+	m_D3D->GetWorldMatrix(worldMatrix);
+	m_D3D->GetProjectionMatrix(projectionMatrix);
+
+	// Pull the model vertex and index buffers on the graphics pipeline to prepare them for drawing.
+	m_Model->Render(m_D3D->GetDeviceContext());
+
+	// Render the model using the color shader.
+	result = m_ColorShader->Render(m_D3D->GetDeviceContext(), m_Model->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix);
+	if (!result)
+	{
+		return false;
+	}
+
+	// 버퍼에 그려진 씬을 화면에 표시합니다.
+	m_D3D->EndScene();
+
+	return true;
+}
